5a2f29dd6a969b7eed91546acec3aa6668d7fd0c
Yusri <muhnuryuusri@gmail.com>
2023-03-15 20:53:50 +0800

    update main activity

--- app/src/main/java/com/example/laboratorium_statistika/MainActivity.kt
+++ app/src/main/java/com/example/laboratorium_statistika/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.laboratorium_statistika
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Xml
+import com.example.laboratorium_statistika.databinding.ActivityMainBinding
+import com.example.laboratorium_statistika.model.Module
+import org.xmlpull.v1.XmlPullParser
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
