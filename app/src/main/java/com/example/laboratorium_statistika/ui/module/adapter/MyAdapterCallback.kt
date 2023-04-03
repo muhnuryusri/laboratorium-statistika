8cf5a29f8d716911a8fd9e02ff4a0c5affa52f0b
Yusri <muhnuryuusri@gmail.com>
2023-04-03 15:41:08 +0800

    delete unused files

--- app/src/main/java/com/example/laboratorium_statistika/ui/module/adapter/MyAdapterCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.example.laboratorium_statistika.ui.module.adapter
-
-import com.example.laboratorium_statistika.model.Module
-import com.example.laboratorium_statistika.model.ModuleTab
-
-interface MyAdapterCallback {
-    fun onModuleClick(id: Int)
-    fun onModuleTabClick(moduleTab: ModuleTab)
-}
