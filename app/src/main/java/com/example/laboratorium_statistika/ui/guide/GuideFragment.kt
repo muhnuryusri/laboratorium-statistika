7cf754b2119a5ecbc061fee5b3664d04ede169c9
Yusri <muhnuryuusri@gmail.com>
2023-05-10 23:53:11 +0800

    update guide

--- app/src/main/java/com/example/laboratorium_statistika/ui/guide/GuideFragment.kt
+++ app/src/main/java/com/example/laboratorium_statistika/ui/guide/GuideFragment.kt
@@ -5,56 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.laboratorium_statistika.R
+import com.example.laboratorium_statistika.databinding.FragmentGuideBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GuideFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GuideFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentGuideBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guide, container, false)
+        binding = FragmentGuideBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GuideFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GuideFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backButtonText.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        val inputStream = requireContext().assets.open("web_view/guide.html")
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        val content = bufferedReader.use { it.readText() }
+
+        binding.webview.loadDataWithBaseURL(null, content, "text/html", "utf-8", null)
     }
 }
